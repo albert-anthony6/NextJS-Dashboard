c20bfae1420025abae96bf5c3a024657a7ce11eb
Albert Valdes <albert.anthony1999@gmail.com>
2024-08-21 11:45:56 -0500

    wip: added image files and nextjs image to about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 export default function AboutPage() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.unsplash.com/photo-1515877389505-58825bad4e5a?q=80&w=1112&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Image for about us."
+          fill
+        />
+      </div>
+    </>
+  );
 }
--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -1,33 +1,62 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
 
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
 export default function Luinks() {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState(false);
+
+  // TODO: (TEMPORARY) Delete later
+  const session = true;
+  const isAdmin = true;
 
   return (
-    <ul className={styles.links}>
-      {links.map((link) => (
-        <li className="link" key={link.title}>
-          <NavLink item={link} key={link.title} />
-        </li>
-      ))}
-    </ul>
+    <div className={styles.container}>
+      <ul className={styles.links}>
+        {links.map((link, idx) => (
+          <li className="link" key={`${idx}-${link.title}`}>
+            <NavLink item={link} />
+          </li>
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </ul>
+      <button onClick={() => setOpen(!open)} className={styles.menuButton}>
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link, idx) => (
+            <NavLink item={link} key={`${idx}-${link.title}`} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
